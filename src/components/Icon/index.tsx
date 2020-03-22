5e068e8c6c675a8977ff9ee6e2b0e08bd98775bb
luciobordonaro <lucio.bordonaro@melfore.com>
2020-03-22 02:31:34 +0100

    Fixed problem with dataCy on Icon

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -4,8 +4,7 @@ import { iconsCatalog } from './utils';
 
 const Icon: FC<IconType> = ({ dataCy, name, size = IconSize.default }) => {
   const icon = iconsCatalog[name];
-  // Argument of type '{ 'data-cy': string; fontSize: IconSize; }' is not assignable to parameter of type 'Attributes & SvgIconProps'.
-  return React.createElement(icon, { 'data-cy': dataCy, fontSize: size });
+  return React.cloneElement(icon, { 'data-cy': dataCy, fontSize: size });
 };
 
 export default Icon;
--- src/components/Icon/utils.tsx
+++ src/components/Icon/utils.tsx
@@ -1,6 +1,7 @@
+import React from 'react';
 import MUISendIcon from '@material-ui/icons/Send';
 import { Icons } from '../../types/Icon';
 
 export const iconsCatalog = {
-  [Icons.send]: MUISendIcon,
+  [Icons.send]: <MUISendIcon />,
 }

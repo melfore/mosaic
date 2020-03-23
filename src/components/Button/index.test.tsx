5531fc82fd1314142c8027de5aa70c09b864f307
luciobordonaro <lucio.bordonaro@melfore.com>
2020-03-24 00:53:55 +0100

    Tests for Button

---
+++ src/components/Button/index.test.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { ButtonType } from '../../types/Button';
+import Button from '.';
+
+const defaultProps = {
+  dataCy: 'myButton',
+  label: 'Click Me',
+  onClick: jest.fn(),
+};
+
+const componentWrapper = (props = {}) => (
+  <Button
+    {...defaultProps}
+    {...props}
+  />
+)
+
+describe('Button test suite:', () => {
+  it('default', () => {
+    const onClickHandler = jest.fn();
+    const component = componentWrapper({
+      onClick: onClickHandler,
+    });
+    const wrapper = mount(component);
+    const button = wrapper.find('button');
+    expect(button.prop('data-cy')).toEqual(defaultProps.dataCy);
+    button.simulate('click');
+    expect(onClickHandler).toHaveBeenCalledTimes(1);
+    const buttonLabel = button.find('span.MuiButton-label');
+    expect(buttonLabel.text()).toEqual(defaultProps.label);
+  })
+})

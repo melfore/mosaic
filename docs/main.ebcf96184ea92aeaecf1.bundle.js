5e83479d0ed66f33ef638cf3d338ec703177aeb4
luciobordonaro <lucio.bordonaro@melfore.com>
2020-03-29 23:42:42 +0200

    Updated docs

---
+++ docs/main.ebcf96184ea92aeaecf1.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],[function(n,o,p){p(470),p(612),p(1259),p(1261),p(1295),n.exports=p(1405)},function(n,o){}],[[0,1,2]]]);
---
+++ docs/precache-manifest.e7e81c401672a60c05685853d4dc0368.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e50292992fa7fcb6949a373b6ce58b3b",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.8a22d70aef83a8cc8e47.bundle.js"
+  },
+  {
+    "url": "runtime~main.8a22d70aef83a8cc8e47.bundle.js"
+  },
+  {
+    "url": "vendors~main.8a22d70aef83a8cc8e47.bundle.js"
+  },
+  {
+    "url": "vendors~main.8a22d70aef83a8cc8e47.bundle.js.LICENSE.txt"
+  }
+]);

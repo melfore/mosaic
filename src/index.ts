f6e4b557f7017ee496ea08b32d565f595610eb5e
luciobordonaro <lucio.bordonaro@melfore.com>
2020-04-08 01:45:19 +0200

    Added exports for InputNumber and fixed exports for types

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
-export { default as Button } from './components/Button';
-export { ButtonIconPosition, ButtonVariants } from './types/Button';
+export { default as Button } from "./components/Button";
+export { ButtonIconPosition, ButtonVariants } from "./types/Button";
 
-export { default as Icon } from './components/Icon';
-export { Icons, IconSize } from './types/Icon';
+export { default as Icon } from "./components/Icon";
+export { Icons, IconSize } from "./types/Icon";
 
-export { default as InputText } from './components/InputText';
-export { InputSize, InputVariant } from './types/InputText';
+export { default as InputText } from "./components/InputText";
+export { default as InputNumber } from "./components/InputNumber";
+export { InputSize, InputVariant } from "./types/Input";

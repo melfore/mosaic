a23ecf07b49ddfd28e0e360aed09dc54b7dbf5a0
luciobordonaro <lucio.bordonaro@melfore.com>
2020-04-09 00:03:57 +0200

    Fixed tests

--- src/components/InputNumber/index.test.tsx
+++ src/components/InputNumber/index.test.tsx
@@ -1,17 +1,25 @@
 import React from "react";
 import { mount } from "enzyme";
+import { InputNumberType } from "../../types/InputNumber";
+import FormMock from "../../utils/FormMock";
 import InputNumber from ".";
 
-const defaultProps = {
+const defaultProps: InputNumberType = {
   dataCy: "input-number",
   label: "Label",
+  onChange: () => {},
+  value: 0,
 };
 
-const componentWrapper = (props = {}) => <InputNumber {...defaultProps} {...props} />;
+const componentWrapper = ({ onChange, value, ...props }: InputNumberType) => (
+  <FormMock onInputChange={onChange!} inputValue={value!}>
+    <InputNumber {...defaultProps} {...props} />
+  </FormMock>
+);
 
 describe("InputNumber test suite:", () => {
   it("default", () => {
-    const component = componentWrapper();
+    const component = componentWrapper(defaultProps);
     const wrapper = mount(component);
     const input = wrapper.find("input");
     expect(input.prop("data-cy")).toEqual(defaultProps.dataCy);
@@ -21,6 +29,7 @@ describe("InputNumber test suite:", () => {
   it("onChange handler", () => {
     const onChangeHandler = jest.fn();
     const component = componentWrapper({
+      ...defaultProps,
       onChange: onChangeHandler,
     });
     const wrapper = mount(component);
@@ -31,9 +40,10 @@ describe("InputNumber test suite:", () => {
 
   it("with bounds", () => {
     const component = componentWrapper({
-      initialValue: 5,
+      ...defaultProps,
       minValue: 0,
       maxValue: 9,
+      value: 5,
     });
     const wrapper = mount(component);
     const input = wrapper.find("input");
@@ -47,15 +57,28 @@ describe("InputNumber test suite:", () => {
 
   it("with float", () => {
     const component = componentWrapper({
-      initialValue: 5.5,
+      ...defaultProps,
       integer: false,
       minValue: 0,
       maxValue: 9,
+      value: 5.5,
     });
     const wrapper = mount(component);
     const input = wrapper.find("input");
     input.simulate("change", { target: { value: "6.5" } });
     const updatedInput = wrapper.find("input");
     expect(updatedInput.prop("value")).toBe(6.5);
+  });
+
+  it("with invalid value", () => {
+    const component = componentWrapper({
+      ...defaultProps,
+      value: null,
+    });
+    const wrapper = mount(component);
+    const input = wrapper.find("input");
+    input.simulate("change", { target: { value: "ajeje" } });
+    const updatedInput = wrapper.find("input");
+    expect(updatedInput.prop("value")).toBe("");
   });
 });

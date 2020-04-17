ccc86263eee78be77f4f6342142a4f5459f72877
Massimo Mauri <massimo.mauri@melfore.com>
2020-04-17 21:49:37 +0200

    add missing documentation for all props

--- src/components/Checkbox/index.stories.tsx
+++ src/components/Checkbox/index.stories.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { action } from "@storybook/addon-actions";
-import { boolean, text } from "@storybook/addon-knobs";
+import { boolean, text, optionsKnob as options, select } from "@storybook/addon-knobs";
 import { DOCS_PAGE_STRUCTURE, StoriesWrapper } from "../../utils/stories";
 import Checkbox from ".";
 import { CheckboxSize } from "../../types/Checkbox";
@@ -18,6 +18,10 @@ export const Canvas = () => (
     dataCy={text("data-cy", "checkbox-identifier")}
     value={boolean("value", false)}
     onChange={action("Change checkbox")}
+    required={boolean("value", false)}
+    size={select("size", CheckboxSize, CheckboxSize.small, CheckboxSize.default)}
+    disabled={boolean("disabled", false)}
+    intermediate={boolean("intermediate", false)}
   />
 );
 

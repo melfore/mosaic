ea8f0eb02b50c7657227ff3994c1449a9880d7a5
luciobordonaro <lucio.bordonaro@melfore.com>
2020-04-21 00:32:21 +0200

    Added icons and auxiliary forwarded type for Icon

--- src/components/Icon/utils.tsx
+++ src/components/Icon/utils.tsx
@@ -1,19 +1,31 @@
 import React from "react";
 import MUIAddIcon from "@material-ui/icons/Add";
+import MUIArrowUpwardIcon from "@material-ui/icons/ArrowUpward";
+import MUIChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import MUIChevronRightIcon from "@material-ui/icons/ChevronRight";
 import MUICloseIcon from "@material-ui/icons/Close";
 import MUIDeleteIcon from "@material-ui/icons/Delete";
 import MUIEditIcon from "@material-ui/icons/Edit";
+import MUIFirstPageIcon from "@material-ui/icons/FirstPage";
+import MUILastPageIcon from "@material-ui/icons/LastPage";
 import MUIOpenInNew from "@material-ui/icons/OpenInNew";
 import MUIPlaylistAddCheckIcon from "@material-ui/icons/PlaylistAddCheck";
+import MUISearchIcon from "@material-ui/icons/Search";
 import MUISendIcon from "@material-ui/icons/Send";
 import { Icons } from "../../types/Icon";
 
 export const iconsCatalog = {
   [Icons.add]: <MUIAddIcon />,
+  [Icons.arrowUp]: <MUIArrowUpwardIcon />,
   [Icons.close]: <MUICloseIcon />,
   [Icons.delete]: <MUIDeleteIcon />,
   [Icons.edit]: <MUIEditIcon />,
+  [Icons.first]: <MUIFirstPageIcon />,
+  [Icons.last]: <MUILastPageIcon />,
+  [Icons.next]: <MUIChevronRightIcon />,
   [Icons.openInNew]: <MUIOpenInNew />,
   [Icons.playlistAddCheck]: <MUIPlaylistAddCheckIcon />,
+  [Icons.prev]: <MUIChevronLeftIcon />,
+  [Icons.search]: <MUISearchIcon />,
   [Icons.send]: <MUISendIcon />,
 };
--- src/types/Icon.ts
+++ src/types/Icon.ts
@@ -2,11 +2,17 @@ import { BaseType } from "./Base";
 
 export enum Icons {
   add = "add",
+  arrowUp = "arrowUp",
   close = "close",
   delete = "delete",
   edit = "edit",
+  first = "first",
+  last = "last",
+  next = "next",
   openInNew = "openInNew",
   playlistAddCheck = "playlistAddCheck",
+  prev = "prev",
+  search = "search",
   send = "send",
 }
 
@@ -16,7 +22,14 @@ export enum IconSize {
   small = "small",
 }
 
+interface IconForwardedType {
+  className?: string;
+  color?: string;
+  ref?: any;
+}
+
 export interface IconType extends BaseType {
+  forwarded?: IconForwardedType;
   name: Icons;
   size?: IconSize;
 }

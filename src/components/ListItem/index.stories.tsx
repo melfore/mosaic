c0ae8c02ae5743dbb43975750bce7dad648cbeac
luciobordonaro <lucio.bordonaro@melfore.com>
2020-05-28 17:57:17 +0200

    Added ListItem component

---
+++ src/components/ListItem/index.stories.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { action } from "@storybook/addon-actions";
+import { text, boolean, select } from "@storybook/addon-knobs";
+import { Icons } from "../../types/Icon";
+import { TypographyVariants } from "../../types/Typography";
+import { getDocsPageStructure, StoriesWrapper } from "../../utils/stories";
+import ListItem from ".";
+
+export default {
+  title: "ListItem",
+  component: ListItem,
+  parameters: {
+    ...getDocsPageStructure("ListItem", false),
+  },
+};
+
+export const Canvas = () => (
+  <ListItem
+    dataCy="list-item"
+    dense={boolean("dense", false)}
+    icon={select("icon", Icons, undefined)}
+    loading={boolean("loading", false)}
+    onClick={action("On ListItem click")}
+    selected={boolean("selected", false)}
+    title={text("title", "ListItem Title")}
+    titleVariant={select("titleVariant", TypographyVariants, undefined)}
+  />
+);
+
+export const Basic = () => (
+  <StoriesWrapper>
+    <ListItem title="Basic List Item" />
+  </StoriesWrapper>
+);
+
+export const Dense = () => (
+  <StoriesWrapper>
+    <ListItem title="Basic List Item" />
+    <ListItem dense title="Basic List Item" />
+  </StoriesWrapper>
+);
+
+export const Loading = () => (
+  <StoriesWrapper>
+    <ListItem loading title="Basic List Item" />
+    <ListItem icon={Icons.add} loading title="Basic List Item" />
+  </StoriesWrapper>
+);
+
+export const Selected = () => (
+  <StoriesWrapper>
+    <ListItem selected title="Basic List Item" />
+  </StoriesWrapper>
+);
+
+export const WithIcon = () => (
+  <StoriesWrapper>
+    <ListItem icon={Icons.add} title="Basic List Item" />
+  </StoriesWrapper>
+);
+
+export const WithTitleVariant = () => (
+  <StoriesWrapper>
+    <ListItem icon={Icons.add} title="Basic List Item" titleVariant={TypographyVariants.caption} />
+  </StoriesWrapper>
+);

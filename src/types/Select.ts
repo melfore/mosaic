0f3dca0f48a11859041595a8bca5add9b698ad5b
luciobordonaro <lucio.bordonaro@melfore.com>
2020-06-01 19:53:07 +0200

    Added Select type with generic

---
+++ src/types/Select.ts
@@ -0,0 +1,17 @@
+import { LoadableType } from "./Base";
+import { ReactNode } from "react";
+
+export interface SelectType<T> extends LoadableType {
+  autoComplete?: boolean;
+  customOptionRendering?: (option: T, selected: boolean) => ReactNode;
+  disabled?: boolean;
+  getGroupLabel?: (groupName: string) => string;
+  getOptionLabel: (option: T) => string;
+  getOptionSelected?: (option: T, value: T) => boolean;
+  groupBy?: (option: T) => string;
+  label?: string;
+  multiple?: boolean;
+  onChange: (value: T | T[] | null) => void;
+  options: T[];
+  value: T | T[] | null;
+}

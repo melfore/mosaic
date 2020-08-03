67fb8240db6373a1d3d4887b3e89ae2472244768
luciobordonaro <lucio.bordonaro@melfore.com>
2020-08-03 16:09:16 +0200

    Tests for Icon

---
+++ src/components/Icon/index.test.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import { IIcon, Icons, IconSize } from "../../types/Icon";
+import { getTestable } from "../../utils/tests";
+import Icon, { DATA_CY_DEFAULT } from ".";
+
+const defaultProps: IIcon = {
+  name: Icons.add,
+};
+
+const getIconTestable = (props?: IIcon, dataCy = DATA_CY_DEFAULT) =>
+  getTestable(Icon, { dataCy, domNode: "svg", props: { ...defaultProps, ...props } });
+
+// TODO: improve tests
+
+describe("Icon test suite:", () => {
+  it("default", () => {
+    const { element, wrapper } = getIconTestable();
+    expect(wrapper).toHaveLength(1);
+    expect(wrapper.childAt(0).matchesElement(<path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z" />));
+
+    const snapshotWrapper = renderer.create(element).toJSON();
+    expect(snapshotWrapper).toMatchSnapshot();
+  });
+
+  it("dataCy", () => {
+    const { element, wrapper } = getIconTestable({ ...defaultProps, dataCy: "custom" }, "custom");
+    expect(wrapper).toHaveLength(1);
+
+    const snapshotWrapper = renderer.create(element).toJSON();
+    expect(snapshotWrapper).toMatchSnapshot();
+  });
+
+  it("loading", () => {
+    const { element, wrapper } = getIconTestable({ ...defaultProps, loading: true });
+
+    const snapshotWrapper = renderer.create(element).toJSON();
+    expect(snapshotWrapper).toMatchSnapshot();
+  });
+
+  it("size", () => {
+    const { element, wrapper } = getIconTestable({ ...defaultProps, size: IconSize.large });
+    expect(wrapper.hasClass("MuiSvgIcon-fontSizeLarge"));
+
+    const snapshotWrapper = renderer.create(element).toJSON();
+    expect(snapshotWrapper).toMatchSnapshot();
+  });
+});

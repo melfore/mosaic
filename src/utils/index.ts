be6e3068057b0a947ae10608c8e0eb8757756248
luciobordonaro <lucio.bordonaro@melfore.com>
2020-08-03 16:14:36 +0200

    Removed useless fn

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,12 +2,6 @@ import { ChangeEvent, MouseEvent } from "react";
 
 export const getComposedDataCy = (extDataCy: string, component: string): string => `${extDataCy}-${component}`;
 
-const slugify = (value: string) =>
-  value
-    .toLowerCase()
-    .replace(/\s+/g, "-")
-    .replace(/[^\w-]+/g, "");
-
 export const suppressEvent = (event: ChangeEvent<any> | MouseEvent) => {
   event.preventDefault();
   event.stopPropagation();

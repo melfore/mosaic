5ffc08b5c0027a84e25058f17211784754967a50
luciobordonaro <lucio.bordonaro@melfore.com>
2020-08-07 18:41:35 +0200

    test: 💍 Restored overall test coverage to 80%

--- .jest/config.js
+++ .jest/config.js
@@ -8,13 +8,12 @@ module.exports = {
   },
   collectCoverageFrom: ["**/*.{js,jsx,ts,tsx}", "!**/*.stories.{js,jsx,ts,tsx}", "!index.ts", "!utils/stories/*"],
   coverageDirectory: "<rootDir>/../coverage",
-  // TODO: complete tests
   coverageThreshold: {
     global: {
-      branches: 60,
-      functions: 60,
-      lines: 60,
-      statements: 60,
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
     },
   },
 };

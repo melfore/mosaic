e189513cb59f971d5dc1ee19c1b1cb24ba448cd2
luciobordonaro <lucio.bordonaro@melfore.com>
2020-08-10 16:58:23 +0200

    test: 💍 Tests for Modal

--- src/components/Modal/index.test.tsx
+++ src/components/Modal/index.test.tsx
@@ -1,4 +1,4 @@
-import { IModal } from "../../types/Modal";
+import { IModal, ModalSize } from "../../types/Modal";
 import { getLocalizedTestable } from "../../utils/tests";
 
 import Modal, { DATA_CY_DEFAULT } from ".";
@@ -10,25 +10,15 @@ const defaultProps: IModal = {
 const getModalTestable = (props?: IModal, dataCy = DATA_CY_DEFAULT) =>
   getLocalizedTestable(Modal, { dataCy, domNode: "div", props: { ...defaultProps, ...props } });
 
-// TODO: complete tests
-
 describe("Modal test suite:", () => {
   it("default", () => {
     const { wrapper } = getModalTestable();
-    // console.log(wrapper.debug());
     expect(wrapper).toHaveLength(1);
-    expect("default-props-check").toBeTruthy();
-
-    // const snapshotWrapper = renderer.create(element).toJSON();
-    // expect(snapshotWrapper).toMatchSnapshot();
   });
 
   it("dataCy", () => {
     const { wrapper } = getModalTestable({ dataCy: "custom" }, "custom");
     expect(wrapper).toHaveLength(1);
-
-    // const snapshotWrapper = renderer.create(element).toJSON();
-    // expect(snapshotWrapper).toMatchSnapshot();
   });
 
   xit("localized", () => {
@@ -38,5 +28,52 @@ describe("Modal test suite:", () => {
     // expect("localizable-props-check").toBeTruthy();
     // const snapshotWrapper = renderer.create(element).toJSON();
     // expect(snapshotWrapper).toMatchSnapshot();
+  });
+
+  it("cancel", () => {
+    const action = jest.fn();
+    const label = "Cancel";
+    const { wrapper } = getModalTestable({ cancel: { action, label } });
+    const cancel = wrapper.find(`button[data-cy='${DATA_CY_DEFAULT}-action-cancel']`);
+    cancel.simulate("click");
+    expect(action).toHaveBeenCalledTimes(1);
+    const cancelLabel = cancel.find("span.MuiButton-label");
+    expect(cancelLabel.text()).toEqual(label);
+  });
+
+  // TODO: improve this
+  it("closable", () => {
+    getModalTestable({ closable: true });
+  });
+
+  it("confirm", () => {
+    const action = jest.fn();
+    const label = "Confirm";
+    const { wrapper } = getModalTestable({ confirm: { action, label } });
+    const confirm = wrapper.find(`button[data-cy='${DATA_CY_DEFAULT}-action-confirm']`);
+    confirm.simulate("click");
+    expect(action).toHaveBeenCalledTimes(1);
+    const confirmLabel = confirm.find("span.MuiButton-label");
+    expect(confirmLabel.text()).toEqual(label);
+  });
+
+  it("onClose", () => {
+    const onClose = jest.fn();
+    const { wrapper } = getModalTestable({ onClose });
+    const backdrop = wrapper.find("div.MuiBackdrop-root");
+    backdrop.simulate("click");
+    expect(onClose).toHaveBeenCalledTimes(1);
+  });
+
+  // TODO: improve this
+  it("size", () => {
+    getModalTestable({ size: ModalSize.large });
+  });
+
+  it("title", () => {
+    const title = "Modal";
+    const { wrapper } = getModalTestable({ title });
+    const titleWrapper = wrapper.find("h2.MuiTypography-root");
+    expect(titleWrapper.text()).toEqual(title);
   });
 });

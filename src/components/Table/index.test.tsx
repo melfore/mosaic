a028c6c3de9ac0bdd9fe338927b2e030c7d1167e
luciobordonaro <lucio.bordonaro@melfore.com>
2020-10-21 16:51:05 +0200

    test: 💍 Added tests for Table selection
    ✅ Closes: 156

--- src/components/Table/index.test.tsx
+++ src/components/Table/index.test.tsx
@@ -53,6 +53,38 @@ describe("Table test suite:", () => {
     });
   });
 
+  it("bulk selection - select all", () => {
+    const onSelectionChange = jest.fn();
+    const { element, wrapper } = getTableTestable({ ...defaultProps, onSelectionChange });
+    const bulkSelection = wrapper.find(`[data-cy='${DATA_CY_DEFAULT}-select-all']`);
+    const bulkSelectionInput = bulkSelection.find("input");
+    bulkSelectionInput.simulate("change", { target: { checked: true } });
+    expect(onSelectionChange).toHaveBeenCalledTimes(1);
+    expect(onSelectionChange).toHaveBeenCalledWith([
+      { id: 0, name: "Mosaic" },
+      { id: 1, name: "Murales" },
+      { id: 2, name: "Paintings" },
+      { id: 3, name: "Photography" },
+      { id: 4, name: "Sculpture" },
+    ]);
+
+    const snapshotWrapper = renderer.create(element).toJSON();
+    expect(snapshotWrapper).toMatchSnapshot();
+  });
+
+  it("bulk selection - select none", () => {
+    const onSelectionChange = jest.fn();
+    const { element, wrapper } = getTableTestable({ ...defaultProps, onSelectionChange });
+    const bulkSelection = wrapper.find(`[data-cy='${DATA_CY_DEFAULT}-select-all']`);
+    const bulkSelectionInput = bulkSelection.find("input");
+    bulkSelectionInput.simulate("change", { target: { checked: false } });
+    expect(onSelectionChange).toHaveBeenCalledTimes(1);
+    expect(onSelectionChange).toHaveBeenCalledWith([]);
+
+    const snapshotWrapper = renderer.create(element).toJSON();
+    expect(snapshotWrapper).toMatchSnapshot();
+  });
+
   it("global action", () => {
     const callback = jest.fn();
     const label = "Account";

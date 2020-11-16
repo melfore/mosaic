b956dfe6a663132542c9228d45936fa226b2ff9b
luciobordonaro <lucio.bordonaro@melfore.com>
2020-11-16 11:42:24 +0100

    feat: 🎸 Added label and localize support to Checkbox
    Checkbox component now supports localized label using the
    FormControlLabel component from material-ui
    
    BREAKING CHANGE: 🧨 Usage of FormControlLabel moved the root dataCy one level above, due to
    this the selector for the input needs the suffix '-input'
    
    ✅ Closes: 172

--- src/types/Checkbox.ts
+++ src/types/Checkbox.ts
@@ -1,13 +1,14 @@
-import { IBase } from "./Base";
+import { ILocalizable } from "./Base";
 
 export enum CheckboxSize {
   small = "small",
   default = "medium",
 }
 
-export interface ICheckbox extends IBase {
+export interface ICheckbox extends ILocalizable {
   disabled?: boolean;
   intermediate?: boolean;
+  label?: string;
   onChange?: (checked: boolean) => void;
   required?: boolean;
   size?: CheckboxSize;

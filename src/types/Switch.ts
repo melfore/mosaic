afd74266990f6c25e9dc6cd9ea28d18c0cb575fa
luciobordonaro <lucio.bordonaro@melfore.com>
2020-11-16 12:16:03 +0100

    feat: 🎸 Added label and localize support to Switch
    Switch component now supports localized label using the FormControlLabel
    component from material-ui
    
    BREAKING CHANGE: 🧨 Usage of FormControlLabel moved the root dataCy one level above, due to
    this the selector for the input needs the suffix '-input'
    
    ✅ Closes: 172, 97

--- src/types/Switch.ts
+++ src/types/Switch.ts
@@ -1,12 +1,13 @@
-import { IBase } from "./Base";
+import { ILocalizable } from "./Base";
 
 export enum SwitchSize {
   small = "small",
   default = "medium",
 }
 
-export interface ISwitch extends IBase {
+export interface ISwitch extends ILocalizable {
   disabled?: boolean;
+  label?: string;
   onChange?: (checked: boolean) => any | void;
   required?: boolean;
   size?: SwitchSize;

cb5660d0d65f26dae6ea6461aa98e362095797e4
luciobordonaro <lucio.bordonaro@melfore.com>
2021-06-15 17:11:08 +0200

    feat: 🎸 [IconButton] Added icon subpart
    Added "icon" subpart to allow retrieval of icon component

--- src/components/IconButton/index.tsx
+++ src/components/IconButton/index.tsx
@@ -3,10 +3,16 @@ import MUIIconButton from "@material-ui/core/IconButton";
 
 import { IconSize } from "../../types/Icon";
 import { IIconButton } from "../../types/IconButton";
-import { suppressEvent } from "../../utils";
+import { getComposedDataCy, suppressEvent } from "../../utils";
 import Icon from "../Icon";
 
 export const DATA_CY_DEFAULT = "icon-button";
+
+export const SUBPARTS_MAP = {
+  icon: {
+    label: "Icon",
+  },
+};
 
 const IconButton: FC<IIconButton> = ({
   dataCy = DATA_CY_DEFAULT,
@@ -26,7 +32,7 @@ const IconButton: FC<IIconButton> = ({
 
   return (
     <MUIIconButton color="inherit" data-cy={dataCy} disabled={disabled} onClick={onClickHandler} style={style}>
-      <Icon dataCy={`${dataCy}-icon`} name={icon} size={size} />
+      <Icon dataCy={getComposedDataCy(dataCy, SUBPARTS_MAP.icon)} name={icon} size={size} />
     </MUIIconButton>
   );
 };

c8f9c6d663814182d10295b72ccd0912cfd54691
luciob <lucio.bordonaro@gmail.com>
2021-06-29 12:10:58 +0200

    feat: 🎸 [AppBar] Added locale support using Menu component
    ✅ Closes: 196

--- src/components/AppBar/index.tsx
+++ src/components/AppBar/index.tsx
@@ -1,48 +1,40 @@
-import React, { FC, Fragment, useMemo, useState } from "react";
-import {
-  AppBar as MUIAppBar,
-  Menu as MUIMenu,
-  MenuItem as MUIMenuItem,
-  PopoverOrigin as MUIPopoverOrigin,
-  Toolbar as MUIToolbar,
-  useTheme,
-} from "@material-ui/core";
+import React, { FC } from "react";
+import { useMemo } from "react";
+import { AppBar as MUIAppBar, Toolbar as MUIToolbar, useTheme } from "@material-ui/core";
 
 import { IAppBar } from "../../types/AppBar";
-import { ButtonIconPosition } from "../../types/Button";
 import { Icons } from "../../types/Icon";
+import { IMenu } from "../../types/Menu";
 import { TypographyVariants } from "../../types/Typography";
 import { getComposedDataCy, suppressEvent } from "../../utils";
 import localized, { ILocalizableProperty } from "../../utils/hocs/localized";
-import Button from "../Button";
+import { logWarn } from "../../utils/logger";
 import IconButton from "../IconButton";
+import Menu from "../Menu";
 import Typography from "../Typography";
-
-const MENU_ITEMS_ANCHORING: MUIPopoverOrigin = {
-  vertical: "top",
-  horizontal: "right",
-};
 
 export const DATA_CY_DEFAULT = "appbar";
 export const DATA_CY_SHORTCUT = "title";
 export const LOCALIZABLE_PROPS: ILocalizableProperty[] = [
   { name: DATA_CY_SHORTCUT, type: "string" },
   { name: "userMenu.label", type: "any[]" },
+  // TODO: review for localized hoc, adding "user.items.label"
 ];
 
 export const SUBPARTS_MAP = {
-  menu: {
-    label: "Menu",
-  },
-  titleClickable: {
-    label: "Title Clickable",
-  },
-  titleText: {
-    label: "Title Text",
-  },
   action: {
     label: "Action (at index n)",
     value: (n = `{n}`) => `action-${n}`,
+  },
+  localesMenu: {
+    label: "Locales Menu",
+  },
+  localesMenuItem: {
+    label: "Locales Menu Item (at index n)",
+    value: (n = `{n}`) => `locales-menu-item-${n}`,
+  },
+  menu: {
+    label: "Menu",
   },
   userMenu: {
     label: "User Menu",
@@ -51,40 +43,48 @@ export const SUBPARTS_MAP = {
     label: "User Menu Item (at index n)",
     value: (n = `{n}`) => `user-menu-item-${n}`,
   },
+  titleClickable: {
+    label: "Title Clickable",
+  },
+  titleText: {
+    label: "Title Text",
+  },
 };
 
 const AppBar: FC<IAppBar> = ({
   actions = [],
   dataCy = DATA_CY_DEFAULT,
+  locale,
   menu,
   onTitleClick,
   style,
   title,
+  user,
   userMenu = [],
   username,
 }) => {
   const theme = useTheme();
 
-  const [userMenuAnchor, setUserMenuAnchor] = useState<any>(null);
+  const userProps = useMemo((): IMenu | undefined => {
+    if (user) {
+      return { ...user };
+    }
 
-  const userMenuButton = useMemo(() => {
-    const userMenuDataCy = getComposedDataCy(dataCy, SUBPARTS_MAP.userMenu);
-    const userMenuIcon: Icons = Icons.account;
-    const userMenuOnClickCallback = () =>
-      setUserMenuAnchor(document.querySelector(`button[data-cy='${userMenuDataCy}']`));
+    if (!userMenu || !userMenu.length) {
+      return;
+    }
 
-    return !username ? (
-      <IconButton dataCy={userMenuDataCy} icon={userMenuIcon} onClick={userMenuOnClickCallback} />
-    ) : (
-      <Button
-        dataCy={userMenuDataCy}
-        icon={{ name: userMenuIcon, position: ButtonIconPosition.right }}
-        label={username}
-        onClick={userMenuOnClickCallback}
-        style={{ textTransform: "lowercase" }}
-      />
+    logWarn(
+      "AppBar",
+      'Deprecation notice, "userMenu" and "username" will be deprecated in favour of "user". Please update your code.'
     );
-  }, [dataCy, username]);
+
+    const userMenuItems = userMenu.map((menuItem) => ({ ...menuItem, value: menuItem.label }));
+    return {
+      items: userMenuItems,
+      label: username,
+    };
+  }, [user, userMenu, username]);
 
   return (
     <MUIAppBar data-cy={dataCy} position="sticky" style={style}>
@@ -123,33 +123,16 @@ const AppBar: FC<IAppBar> = ({
               style={{ marginRight: `${theme.spacing(0.5)}px`, ...style }}
             />
           ))}
-          {userMenu && userMenu.length > 0 && (
-            <Fragment>
-              {userMenuButton}
-              <MUIMenu
-                id={`${dataCy}-user-menu`}
-                anchorEl={userMenuAnchor}
-                anchorOrigin={MENU_ITEMS_ANCHORING}
-                keepMounted
-                transformOrigin={MENU_ITEMS_ANCHORING}
-                open={!!userMenuAnchor}
-                onClose={() => setUserMenuAnchor(null)}
-              >
-                {userMenu.map(({ label, onClick }, index) => (
-                  <MUIMenuItem
-                    key={`user-menu-${index}`}
-                    data-cy={getComposedDataCy(dataCy, SUBPARTS_MAP.userMenuItem, index)}
-                    onClick={(event) => {
-                      suppressEvent(event);
-                      setUserMenuAnchor(null);
-                      onClick();
-                    }}
-                  >
-                    {label}
-                  </MUIMenuItem>
-                ))}
-              </MUIMenu>
-            </Fragment>
+          {locale && (
+            <Menu dataCy={getComposedDataCy(dataCy, SUBPARTS_MAP.localesMenu)} icon={Icons.language} {...locale} />
+          )}
+          {userProps && (
+            <Menu
+              dataCy={getComposedDataCy(dataCy, SUBPARTS_MAP.userMenu)}
+              icon={Icons.account}
+              style={{ textTransform: "lowercase" }}
+              {...userProps}
+            />
           )}
         </div>
       </MUIToolbar>
--- src/types/AppBar.ts
+++ src/types/AppBar.ts
@@ -1,12 +1,15 @@
 import { ILocalizable } from "./Base";
 import { IBaseButton } from "./Button";
 import { IBaseIconButton } from "./IconButton";
+import { IMenu } from "./Menu";
 
 export interface IAppBar extends ILocalizable {
   actions?: IBaseIconButton[];
+  locale?: IMenu;
   menu?: IBaseIconButton;
   onTitleClick?: () => void;
   title?: string;
+  user?: IMenu;
   userMenu?: IBaseButton[];
   username?: string;
 }

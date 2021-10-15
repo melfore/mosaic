dd6682907fbe319249403762c5264cf52a2920ac
luciob <lucio.bordonaro@gmail.com>
2021-10-15 14:23:26 +0200

    docs: ✏️ [Avatar] Migrated stories to SB v6.x
    ✅ Closes: 134

--- src/components/Avatar/index.stories.tsx
+++ src/components/Avatar/index.stories.tsx
@@ -2,21 +2,22 @@ import React from "react";
 import MUIStyleIcon from "@material-ui/icons/Style";
 import { ComponentMeta, ComponentStory } from "@storybook/react";
 
-// import { AvatarVariant } from "../../types/Avatar";
 import { Icons } from "../../types/Icon";
 import { getAllComposedDataCy } from "../../utils";
-import { getDocsPage } from "../../utils/stories";
+import getDocsPage from "../../utils/stories";
 
 import Avatar, { DATA_CY_DEFAULT, SUBPARTS_MAP } from ".";
 
-// More on argTypes: https://storybook.js.org/docs/react/api/argtypes
 export default {
   title: "Basic/Avatar",
   component: Avatar,
   parameters: {
     docs: {
       ...getDocsPage({
-        basedOn: "@material-ui/core/Avatar",
+        basedOn: {
+          label: "MUI Avatar Component",
+          url: "https://v4.mui.com/components/avatars/",
+        },
         component: "Avatar",
         e2eTestInfo: {
           dataCyDefault: DATA_CY_DEFAULT,
@@ -25,38 +26,53 @@ export default {
       }),
     },
   },
-  argTypes: {},
+  // argTypes: {},
 } as ComponentMeta<typeof Avatar>;
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof Avatar> = (args) => <Avatar {...args} />;
+const Template: ComponentStory<typeof Avatar> = (args) => <Avatar {...args} dataCy="avatar" />;
 
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
-// export const Canvas = () => (
-//   <Avatar
-//     alt={text("alt", "Alt Text")}
-//     dataCy={text("dataCy", "avatar")}
-//     icon={select("icon", Icons, Icons.add)}
-//     loading={boolean("loading", false)}
-//     text={text("text", "Avatar Text")}
-//     variant={select("variant", AvatarVariant, AvatarVariant.default)}
-//   />
-// );
 export const Primary = Template.bind({});
 Primary.args = {
   icon: Icons.add,
 };
 
-export const CustomStyle = () => <Avatar icon={Icons.business} style={{ backgroundColor: "red", color: "white" }} />;
+export const CustomIcon = Template.bind({});
+CustomIcon.args = {
+  icon: <MUIStyleIcon />,
+};
 
-export const Icon = () => <Avatar icon={Icons.business} />;
+export const CustomStyle = Template.bind({});
+CustomStyle.args = {
+  ...Primary.args,
+  style: {
+    backgroundColor: "red",
+    color: "white",
+  },
+};
 
-export const CustomIcon = () => <Avatar icon={<MUIStyleIcon />} />;
+export const Image = Template.bind({});
+Image.args = {
+  src: "//upload.wikimedia.org/wikipedia/commons/thumb/6/60/Roof_hafez_tomb.jpg/440px-Roof_hafez_tomb.jpg",
+};
 
-export const Image = () => (
-  <Avatar src="//upload.wikimedia.org/wikipedia/commons/thumb/6/60/Roof_hafez_tomb.jpg/440px-Roof_hafez_tomb.jpg" />
-);
+export const Loading = Template.bind({});
+Loading.args = {
+  loading: true,
+};
 
-export const Loading = () => <Avatar loading />;
+export const Text = Template.bind({});
+Text.args = {
+  text: "MO",
+};
 
-export const Text = () => <Avatar text="MO" />;
+export const VariantRounded = Template.bind({});
+VariantRounded.args = {
+  ...Primary.args,
+  variant: "rounded",
+};
+
+export const VariantSquare = Template.bind({});
+VariantSquare.args = {
+  ...Primary.args,
+  variant: "square",
+};

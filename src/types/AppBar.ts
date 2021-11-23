e76a508c647fa937a86c95c46ac43ae9cf22c1aa
luciob <lucio.bordonaro@gmail.com>
2021-11-23 15:08:41 +0100

    docs: ✏️ [AppBar] Type documentation
    ✅ Closes: 134

--- src/types/AppBar.ts
+++ src/types/AppBar.ts
@@ -4,12 +4,38 @@ import { IBaseIconButton } from "./IconButton";
 import { IMenu } from "./Menu";
 
 export interface IAppBar extends ILocalizable {
+  /**
+   * Actions available for AppBar
+   */
   actions?: IBaseIconButton[];
+  /**
+   * Locale dedicated menu
+   */
   locale?: IMenu;
+  /**
+   * Main AppBar menu
+   */
   menu?: IBaseIconButton;
+  /**
+   * Callback for click events on AppBar title
+   */
   onTitleClick?: () => void;
+  /**
+   * AppBar title
+   */
   title?: string;
+  /**
+   * User dedicated menu
+   */
   user?: IMenu;
+  /**
+   * User dedicated menu
+   * [@deprecated Use user.items]
+   */
   userMenu?: IBaseButton[];
+  /**
+   * Username to display
+   * [@deprecated Use user.label]
+   */
   username?: string;
 }
--- src/types/Menu.ts
+++ src/types/Menu.ts
@@ -4,13 +4,31 @@ import { IPartialIconUtilizer } from "./Icon";
 export type IMenuItemCallback = (value: string) => void;
 
 export interface IMenuItem {
+  /**
+   * Menu item label
+   */
   label?: string;
+  /**
+   * Callback for click events, dedicated for menu item
+   */
   onClick?: IMenuItemCallback;
+  /**
+   * Value of menu item
+   */
   value: string;
 }
 
 export interface IMenu extends IBase, IPartialIconUtilizer {
+  /**
+   * List of menu items
+   */
   items: IMenuItem[];
+  /**
+   * Menu label
+   */
   label?: string;
+  /**
+   * Callback for click events, applied to all menu items
+   */
   onItemClick?: IMenuItemCallback;
 }

7561bd78ad756f511813d4469d137cc96a101c13
luciob <lucio.bordonaro@gmail.com>
2021-11-23 19:09:53 +0100

    docs: ✏️ [Typography] Migrated stories to SB v6.x
    ✅ Closes: 134

--- src/components/Typography/index.stories.tsx
+++ src/components/Typography/index.stories.tsx
@@ -1,118 +1,115 @@
-// import React from "react";
-// import { boolean, select, text } from "@storybook/addon-knobs";
+import React from "react";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
 
-// import { TypographyDisplay, TypographyVariants } from "../../types/Typography";
-// import IntlProviderMock, { LocaleMock, MessageMock } from "../../utils/mocks/IntlProviderMock";
-// import { StoriesWrapper } from "../../utils/stories";
-// import { getDocumentationPage } from "../../utils/stories";
+import getDocsPage from "../../utils/stories";
 
-// import Typography, { DATA_CY_DEFAULT, LOCALIZABLE_PROPS, TypographyWithProps } from ".";
+import Typography, { DATA_CY_DEFAULT, LOCALIZABLE_PROPS, TypographyWithProps } from ".";
 
-// export default {
-//   title: "Typography",
-//   component: TypographyWithProps,
-//   parameters: {
-//     ...getDocumentationPage({
-//       basedOn: "@material-ui/core/Typography",
-//       component: "Typography",
-//       e2eTestInfo: {
-//         dataCyDefault: DATA_CY_DEFAULT,
-//       },
-//       localizableProps: LOCALIZABLE_PROPS,
-//     }),
-//   },
-// };
+const COMPONENT_NAME = "Typography";
+Typography.displayName = COMPONENT_NAME;
+TypographyWithProps.displayName = COMPONENT_NAME;
 
-// export const Canvas = () => (
-//   <Typography
-//     bottomSpacing={boolean("bottomSpacing", false)}
-//     children={text("children", "Typography example")}
-//     dataCy={text("dataCy", DATA_CY_DEFAULT)}
-//     display={select("display", TypographyDisplay, TypographyDisplay.default)}
-//     loading={boolean("loading", false)}
-//     localized={boolean("localized", false)}
-//     truncated={boolean("truncated", false)}
-//     variant={select("variant", TypographyVariants, TypographyVariants.body)}
-//   />
-// );
+export default {
+  title: "Typography",
+  component: TypographyWithProps,
+  parameters: {
+    docs: {
+      ...getDocsPage({
+        basedOn: {
+          label: "MUI Typography Component",
+          url: "https://v4.mui.com/components/typography/",
+        },
+        component: COMPONENT_NAME,
+        e2eTestInfo: {
+          dataCyDefault: DATA_CY_DEFAULT,
+        },
+        localizableProps: LOCALIZABLE_PROPS,
+      }),
+    },
+  },
+} as ComponentMeta<typeof TypographyWithProps>;
 
-// const allVariantsKeys = Object.keys(TypographyVariants);
-// const allVariantsValues = Object.values(TypographyVariants);
-// const allVariants: { [key: string]: string } = {};
-// allVariantsKeys.forEach((key, index) => (allVariants[key] = allVariantsValues[index]));
-// const getVariant = (variantKey: string) => allVariantsValues.find((vv) => vv === allVariants[variantKey]);
-// const allVariantsJsx = allVariantsKeys.map((variantKey) => (
-//   <div className="typography-wrapper">
-//     <Typography variant={TypographyVariants.overline}>{variantKey}</Typography>
-//     <Typography dataCy={`typography-${variantKey}`} variant={getVariant(variantKey)}>
-//       This is an example text
-//     </Typography>
-//   </div>
-// ));
+const Template: ComponentStory<typeof TypographyWithProps> = (args) => (
+  <Typography {...args} dataCy={DATA_CY_DEFAULT} />
+);
 
-// export const AllVariants = () => <StoriesWrapper>{allVariantsJsx}</StoriesWrapper>;
+export const Primary = Template.bind({});
+Primary.args = {
+  children: "Text example",
+};
 
-// export const BottomSpacing = () => (
-//   <StoriesWrapper>
-//     <div className="typography-wrapper">
-//       <Typography>Some text without bottom spacing</Typography>
-//       <Typography bottomSpacing>Some text with bottom spacing</Typography>
-//       <Typography>Other text</Typography>
-//     </div>
-//   </StoriesWrapper>
-// );
+export const BottomSpacing = Template.bind({});
+BottomSpacing.args = {
+  ...Primary.args,
+  bottomSpacing: true,
+};
 
-// export const CustomStyle = () => (
-//   <Typography style={{ backgroundColor: "red", color: "white" }}>Custom Style</Typography>
-// );
+export const CustomContent = Template.bind({});
+CustomContent.args = {
+  ...Primary.args,
+  children: (
+    <span>
+      <b>Strong</b> text
+    </span>
+  ),
+};
 
-// export const Inline = () => (
-//   <StoriesWrapper>
-//     <div>
-//       <Typography display={TypographyDisplay.inline}>Body&nbsp;</Typography>
-//       <Typography display={TypographyDisplay.inline} variant={TypographyVariants.caption}>
-//         caption&nbsp;
-//       </Typography>
-//       <Typography display={TypographyDisplay.inline} variant={TypographyVariants.label}>
-//         label&nbsp;
-//       </Typography>
-//       <Typography display={TypographyDisplay.inline} variant={TypographyVariants.overline}>
-//         overline
-//       </Typography>
-//     </div>
-//   </StoriesWrapper>
-// );
+export const DisplayBlock = Template.bind({});
+DisplayBlock.args = {
+  ...Primary.args,
+  display: "block",
+};
 
-// export const Loading = () => (
-//   <StoriesWrapper>
-//     <div className="typography-wrapper">
-//       <Typography loading />
-//     </div>
-//   </StoriesWrapper>
-// );
+export const DisplayInline = Template.bind({});
+DisplayInline.args = {
+  ...Primary.args,
+  display: "inline",
+};
 
-// export const Localized = () => (
-//   // IntlProviderMock simulates external IntlProvider context
-//   <IntlProviderMock locale={select("locale", LocaleMock, LocaleMock.en)}>
-//     <Typography localized>{MessageMock.typography}</Typography>
-//   </IntlProviderMock>
-// );
+export const Loading = Template.bind({});
+Loading.args = {
+  ...Primary.args,
+  loading: true,
+};
 
-// export const MixedContents = () => (
-//   <Typography>
-//     <strong>Strong</strong> Normal <i>Italic</i> <u>Underlined</u>
-//   </Typography>
-// );
+export const Truncated = Template.bind({});
+Truncated.args = {
+  ...Primary.args,
+  truncated: true,
+};
 
-// export const Truncated = () => (
-//   <StoriesWrapper>
-//     <div className="typography-wrapper">
-//       <div style={{ width: "50%" }}>
-//         <Typography truncated>Trucates with respect to parent width (resize window to make it happen)</Typography>
-//       </div>
-//       <div style={{ width: "200px" }}>
-//         <Typography truncated>Truncated all chars after 200px</Typography>
-//       </div>
-//     </div>
-//   </StoriesWrapper>
-// );
+export const VariantBody = Template.bind({});
+VariantBody.args = {
+  ...Primary.args,
+  variant: "body",
+};
+
+export const VariantCaption = Template.bind({});
+VariantCaption.args = {
+  ...Primary.args,
+  variant: "caption",
+};
+
+export const VariantLabel = Template.bind({});
+VariantLabel.args = {
+  ...Primary.args,
+  variant: "label",
+};
+
+export const VariantOverline = Template.bind({});
+VariantOverline.args = {
+  ...Primary.args,
+  variant: "overline",
+};
+
+export const VariantPagetitle = Template.bind({});
+VariantPagetitle.args = {
+  ...Primary.args,
+  variant: "pagetitle",
+};
+
+export const VariantTitle = Template.bind({});
+VariantTitle.args = {
+  ...Primary.args,
+  variant: "title",
+};

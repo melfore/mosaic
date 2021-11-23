ce4c484676a41fa22da7f4ebe65560d38e024ca0
luciob <lucio.bordonaro@gmail.com>
2021-11-23 15:47:21 +0100

    docs: ✏️ [Card] Type documentation
    ✅ Closes: 134

--- src/types/Card.ts
+++ src/types/Card.ts
@@ -4,9 +4,24 @@ import { ILoadable, ILocalizable } from "./Base";
 import { IPartialIconUtilizer } from "./Icon";
 
 export interface ICard extends ILoadable, ILocalizable, IPartialIconUtilizer {
+  /**
+   * List of actions available in footer
+   */
   actions?: ReactElement[];
+  /**
+   * Card collapsible content
+   */
   collapsible?: ReactNode;
+  /**
+   * Card title
+   */
   title: string;
+  /**
+   * Card subtitle
+   */
   subtitle?: string;
+  /**
+   * Unmounts content on collapsible closed
+   */
   unmountCollapsible?: boolean;
 }

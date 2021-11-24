b5719374a9c1e81ab171e84bcf06b7b5d5d762c2
luciob <lucio.bordonaro@gmail.com>
2021-11-24 18:14:09 +0100

    docs: ✏️ [Modal] Type documentation
    ✅ Closes: 134

--- src/types/Modal.ts
+++ src/types/Modal.ts
@@ -1,6 +1,9 @@
 import { ILocalizable } from "./Base";
 import { IButton } from "./Button";
 
+/**
+ * @deprecated Use "sm" | "md" | "lg" | "xl"
+ */
 export enum ModalSize {
   default = "md",
   fullScreen = "xl",
@@ -8,16 +11,42 @@ export enum ModalSize {
   small = "sm",
 }
 
+type IModalSize = "sm" | "md" | "lg" | "xl";
+
 type IModalAction = Pick<IButton, "disabled" | "label" | "style" | "variant"> & {
+  /**
+   * Callback for click events on Modal action
+   */
   action: () => void;
 };
 
 export interface IModal extends ILocalizable {
+  /**
+   * Modal cancel button
+   */
   cancel?: IModalAction;
+  /**
+   * Enables closable state
+   */
   closable?: boolean;
+  /**
+   * Modal confirm button
+   */
   confirm?: IModalAction;
+  /**
+   * Callback for close events
+   */
   onClose?: () => void;
+  /**
+   * Enables open state, making Modal appear
+   */
   open?: boolean;
-  size?: ModalSize;
+  /**
+   * Size of Modal dialog
+   */
+  size?: IModalSize | ModalSize;
+  /**
+   * Modal title
+   */
   title?: string;
 }

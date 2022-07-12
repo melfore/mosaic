14f1a1c10f120f3174ffe518dda5360d6491a206
luciob <lucio.bordonaro@gmail.com>
2022-07-12 17:11:02 +0200

    feat: 🎸 [IconButton] Added optional tooltip
    ✅ Closes: 167

--- src/components/IconButton/index.tsx
+++ src/components/IconButton/index.tsx
@@ -1,8 +1,8 @@
-import React, { FC, useCallback } from "react";
-import MUIIconButton from "@material-ui/core/IconButton";
+import React, { FC, useCallback, useMemo } from "react";
+import { IconButton as MUIIconButton, Tooltip as MUITooltip } from "@material-ui/core";
 
 import { IIconButton } from "../../types/IconButton";
-import { getComposedDataCy, suppressEvent } from "../../utils";
+import { getComposedDataCy, slugify, suppressEvent } from "../../utils";
 import IconWrapper from "../IconWrapper";
 
 export const DATA_CY_DEFAULT = "icon-button";
@@ -21,6 +21,7 @@ const IconButton: FC<IIconButton> = ({
   rotate = false,
   size = "medium",
   style,
+  tooltip,
 }) => {
   const onClickHandler = useCallback(
     (event: any) => {
@@ -30,10 +31,23 @@ const IconButton: FC<IIconButton> = ({
     [onClick]
   );
 
+  const iconButton = useMemo(
+    () => (
+      <MUIIconButton color="inherit" data-cy={dataCy} disabled={disabled} onClick={onClickHandler} style={style}>
+        <IconWrapper dataCy={getComposedDataCy(dataCy, SUBPARTS_MAP.icon)} icon={icon} rotate={rotate} size={size} />
+      </MUIIconButton>
+    ),
+    [dataCy, disabled, icon, onClickHandler, rotate, size, style]
+  );
+
+  if (!tooltip) {
+    return iconButton;
+  }
+
   return (
-    <MUIIconButton color="inherit" data-cy={dataCy} disabled={disabled} onClick={onClickHandler} style={style}>
-      <IconWrapper dataCy={getComposedDataCy(dataCy, SUBPARTS_MAP.icon)} icon={icon} rotate={rotate} size={size} />
-    </MUIIconButton>
+    <MUITooltip aria-label={slugify(tooltip)} title={tooltip}>
+      <span>{iconButton}</span>
+    </MUITooltip>
   );
 };
 
--- src/types/IconButton.ts
+++ src/types/IconButton.ts
@@ -2,6 +2,11 @@ import { IClickable } from "./Base";
 import { IIcon, IIconUtilizer } from "./Icon";
 import { IDisablableInput } from "./Input";
 
-export interface IBaseIconButton extends IClickable, IIconUtilizer {}
+export interface IBaseIconButton extends IClickable, IIconUtilizer {
+  /**
+   * Text of tooltip
+   */
+  tooltip?: string;
+}
 
 export type IIconButton = IBaseIconButton & IDisablableInput & Pick<IIcon, "rotate" | "size">;

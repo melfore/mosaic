ec58a78c22a201a792dc469a34ef511a6da641fa
luciob <lucio.bordonaro@gmail.com>
2022-12-05 17:16:38 +0100

    fix: 🐛 [Popper] Mismatch between MUIPopper type: React 17 vs 18

--- src/components/Select/components/Popper/index.tsx
+++ src/components/Select/components/Popper/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useMemo } from "react";
+import React, { CSSProperties, FC, useMemo } from "react";
 import { Popper as MUIPopper, PopperProps as MUIPopperProps } from "@mui/material";
 
 interface ISelectPopper {
@@ -12,10 +12,28 @@ const SelectPopper: FC<ISelectPopper> = ({ forwarded, popperWidth }) => {
   const width = useMemo(() => {
     const anchorElRef = anchorEl as any;
     const anchorElWidth = anchorElRef ? anchorElRef.clientWidth : null;
+
     return !!popperWidth && popperWidth > anchorElWidth ? popperWidth : anchorElWidth;
   }, [popperWidth, anchorEl]);
 
-  return <MUIPopper {...forwarded} placement="bottom-start" style={{ width }} />;
+  const style = useMemo(
+    (): CSSProperties => ({
+      width,
+    }),
+    [width]
+  );
+
+  return (
+    <MUIPopper
+      {...forwarded}
+      placement="bottom-start"
+      style={style}
+      // React 17.x vs React 18.x mismatch
+      // See: https://github.com/mui/material-ui/issues/35287
+      onResize={undefined}
+      onResizeCapture={undefined}
+    />
+  );
 };
 
 export default SelectPopper;

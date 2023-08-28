61f1951318a9ab0bb14dd835ecaa2b7fafed75f9
luciob <lucio.bordonaro@gmail.com>
2023-08-28 18:05:50 +0200

    test: 💍 Added Storybook test-runner
    ✅ Closes: #297

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,11 @@
 module.exports = {
   stories: ["../src/components/**/*.stories.mdx", "../src/components/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-mdx-gfm"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-mdx-gfm",
+    "@storybook/addon-coverage",
+  ],
   features: {
     emotionAlias: false,
   },

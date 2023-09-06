da5f73263b8881944573e3f24675e336cec7daf0
CrisGrud <grudzinski.krzysztof11@gmail.com>
2023-09-06 16:28:54 +0200

    test: 💍 [Tabs] Added test

--- src/components/Tabs/index.test.tsx
+++ src/components/Tabs/index.test.tsx
@@ -15,45 +15,30 @@ const DEFAULT_TEST_OPTIONS: ITestOptions<TabsType> = {
 const getTabsTestable = (options?: IPartialTestOptions<TabsType>) =>
   getTestableComponent(Tabs, DEFAULT_TEST_OPTIONS, options);
 
-describe.skip("Tabs test suite:", () => {
+describe("Tabs test suite:", () => {
   it("color", () => {
-    const { element } = getTabsTestable({ props: { color: "primary" } });
-
-    const snapshotWrapper = renderer.create(element).toJSON();
-    expect(snapshotWrapper).toMatchSnapshot();
+    getTabsTestable({ props: { color: "primary" } });
   });
 
   it("Page", () => {
-    const { element } = getTabsTestable({ props: { labelList: [{ label: "PAGE1" }, { label: "PAGE2" }] } });
-
-    const snapshotWrapper = renderer.create(element).toJSON();
-    expect(snapshotWrapper).toMatchSnapshot();
+    getTabsTestable({ props: { labelList: [{ label: "PAGE1" }, { label: "PAGE2" }] } });
   });
 
   it("Wrapped", () => {
-    const { element } = getTabsTestable({
+    getTabsTestable({
       props: { labelList: [{ label: "PAGE1" }, { label: "PAGE2", wrapped: true }] },
     });
-
-    const snapshotWrapper = renderer.create(element).toJSON();
-    expect(snapshotWrapper).toMatchSnapshot();
   });
 
   it("children", () => {
-    const { element } = getTabsTestable({
+    getTabsTestable({
       props: { labelList: [{ label: "PAGE1", content: "Children" }] },
     });
-
-    const snapshotWrapper = renderer.create(element).toJSON();
-    expect(snapshotWrapper).toMatchSnapshot();
   });
 
   it("orientation", () => {
-    const { element } = getTabsTestable({
+    getTabsTestable({
       props: { labelList: [{ label: "PAGE1" }], orientation: "vertical" },
     });
-
-    const snapshotWrapper = renderer.create(element).toJSON();
-    expect(snapshotWrapper).toMatchSnapshot();
   });
 });

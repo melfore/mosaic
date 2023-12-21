49a8d8c001431f1d25fcc361b9db0db6f0cde437
luciob <lucio.bordonaro@gmail.com>
2023-12-21 15:17:58 +0100

    fix: 🐛 Following official StoryBook setup for @mui

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,3 +1,25 @@
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import "@fontsource/material-icons";
+
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { withThemeFromJSXProvider } from "@storybook/addon-themes";
+
+const lightTheme = createTheme();
+
+export const decorators = [
+  withThemeFromJSXProvider({
+    themes: {
+      light: lightTheme,
+    },
+    defaultTheme: "light",
+    Provider: ThemeProvider,
+    GlobalStyles: CssBaseline,
+  }),
+];
+
 export const parameters = {
   actions: {
     argTypesRegex: "^on[A-Z].*",

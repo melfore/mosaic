91173c5c940c779077f2d084e44d3681bb31ea8a
luciob <lucio.bordonaro@gmail.com>
2024-01-11 12:03:25 +0100

    test: 💍 Removed deprecated postRender method

--- .storybook/test-runner.ts
+++ .storybook/test-runner.ts
@@ -1,7 +1,7 @@
 import type { TestRunnerConfig } from "@storybook/test-runner";
 
 const config: TestRunnerConfig = {
-  async postRender(page, context) {
+  async postVisit(page, context) {
     const elementHandler = await page.$("#storybook-root");
     if (!elementHandler) {
       return;
